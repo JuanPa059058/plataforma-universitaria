235a85dc632d5cb5e1d70fac449361024f56f5d4
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-09-12 14:18:24 -0500

    Setting up

---
+++ src/main/java/com/mycompany/plataformuniversity/PlataformUniversity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.plataformuniversity;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class PlataformUniversity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
